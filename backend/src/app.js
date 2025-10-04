decac3d40683b553a2c7afb987575df922c44088
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2025-10-05 03:26:08 +0600

    Added folders

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+// src/app.js
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("✅ Backend is running!");
+});
+
+export default app;
---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+// server.js
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
