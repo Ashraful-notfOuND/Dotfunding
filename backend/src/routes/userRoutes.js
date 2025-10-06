49b13ec80e731b3a451a78b8be439b44235a4858
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2025-10-06 16:39:38 +0600

    added login

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,5 +4,7 @@ import { signUpUser } from "../controllers/userController.js";
 const router = express.Router();
 
 router.post("/signup", signUpUser);
+router.post("/login", loginUser);
 
 export default router;
+
