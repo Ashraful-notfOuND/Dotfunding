41965643c0e94209a4603f62b124cc1fcef18955
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2025-10-07 14:10:51 +0600

    Add files via upload

---
+++ backend/src/routes/projectRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createProject, getAllProjects } from "../controllers/projectController.js";
+import multer from "multer";
+const upload = multer({ storage: multer.memoryStorage() });
+
+const router = express.Router();
+
+router.post("/create", upload.single("image"), createProject);
+router.get("/all", getAllProjects);
+
+export default router;
