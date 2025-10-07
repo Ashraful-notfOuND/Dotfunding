33a635f97bcedde8c1aec35138bcd510e1165f7d
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2025-10-07 14:55:51 +0600

    for multiple images

--- backend/src/routes/projectRoutes.js
+++ backend/src/routes/projectRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { createProject, getAllProjects } from "../controllers/projectController.js";
 import multer from "multer";
+
 const upload = multer({ storage: multer.memoryStorage() });
 
 const router = express.Router();
 
-router.post("/create", upload.single("image"), createProject);
+
+router.post("/create", upload.array("images"), createProject);
 router.get("/all", getAllProjects);
 
 export default router;
