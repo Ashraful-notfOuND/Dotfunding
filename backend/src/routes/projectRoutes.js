a8ecc06a49fe50ca1350fa4a5153bc86db5452db
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2025-10-27 21:53:49 +0600

    added routes

--- backend/src/routes/projectRoutes.js
+++ backend/src/routes/projectRoutes.js
@@ -10,8 +10,12 @@ const router = express.Router();
 router.post("/create", upload.single("image"), createProject);
 //router.get("/all", getAllProjects);
 router.post("/campaign", upload.array("images"), createCampaign);
+router.post("/faqs", createFAQs);
 router.post("/rewards", express.json(), createRewards);
 router.get("/rewards/:projectId", getRewards);
 router.get("/userProjects/:userId", getUserProjects);
 router.get("/:id", getProjectById);
+router.get("/campaign/:projectId", getCampaignByProjectId);
+router.get("/faqs/:projectId", getFAQsByProjectId);
 export default router;
+
